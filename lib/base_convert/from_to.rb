4e8724df35146461eb82a11c9c7cdc11d07f80ba
carlosjhr64 <carlosjhr64@gmail.com>
2019-12-13 10:40:42 -0800

    simple and fast validations for FromTo

--- lib/base_convert/from_to.rb
+++ lib/base_convert/from_to.rb
@@ -4,6 +4,11 @@ class FromTo
   include BaseConvert
 
   def initialize(base: 10, to_base: 16, digits: WORD, to_digits: digits)
+    base      = BASE[base]         if base.is_a?      Symbol
+    to_base   = BASE[to_base]      if to_base.is_a?   Symbol
+    digits    = DIGITS[digits]     if digits.is_a?    Symbol
+    to_digits = DIGITS[to_digits]  if to_digits.is_a? Symbol
+    raise "base must cover digits" if base > digits.length or to_base > to_digits.length
     @base, @to_base, @digits, @to_digits = base, to_base, digits, to_digits
   end
  

7fe9f285928a21e89a35d753f2e8430436b2e8df
carlosjhr64 <carlosjhr64@gmail.com>
2019-12-13 13:15:17 -0800

    configuration tests

--- lib/base_convert/configuration.rb
+++ lib/base_convert/configuration.rb
@@ -19,19 +19,35 @@ module Configuration
     :unambiguous => UNAMBIGUOUS.length,
     :hexadecimal => 16,
     :hex         => 16,
+    :h           => 16,
     :decimal     => 10,
     :dec         => 10,
+    :d           => 10,
     :octal       => 8,
     :oct         => 8,
+    :o           => 8,
     :binary      => 2,
+    :bin         => 2,
+    :b           => 2,
   }
+
+  BASE[:g]  = BASE[:graph]
+  BASE[:q]  = BASE[:qgraph]
+  BASE[:w_] = BASE[:word_]
+  BASE[:w]  = BASE[:word]
+  BASE[:u]  = BASE[:unambiguous]
 
   DIGITS = {
     :graph       => GRAPH,
+    :g           => GRAPH,
     :qgraph      => QGRAPH,
-    :word_       => WORD,
+    :q           => QGRAPH,
+    :word_       => WORD_,
+    :w_          => WORD_,
     :word        => WORD,
+    :w           => WORD,
     :unambiguous => UNAMBIGUOUS,
+    :u           => UNAMBIGUOUS,
   }
 
 end

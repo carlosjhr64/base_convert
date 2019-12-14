8ddbfff242dc1d5881288581ce143f4410b818c3
carlosjhr64 <carlosjhr64@gmail.com>
2019-12-14 14:05:53 -0800

    FromTo tests

--- base_convert.gemspec
+++ base_convert.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
 
   s.name     = 'base_convert'
-  s.version  = '3.0.191213'
+  s.version  = '3.0.191214'
 
   s.homepage = 'https://github.com/carlosjhr64/base_convert'
 
--- lib/base_convert/from_to.rb
+++ lib/base_convert/from_to.rb
@@ -3,12 +3,12 @@ class FromTo
   include Configuration
   include BaseConvert
 
-  def initialize(base: 10, to_base: 16, digits: WORD, to_digits: digits)
+  def initialize(base: 10, to_base: base, digits: WORD, to_digits: digits)
     base      = BASE[base]         if base.is_a?      Symbol
     to_base   = BASE[to_base]      if to_base.is_a?   Symbol
     digits    = DIGITS[digits]     if digits.is_a?    Symbol
     to_digits = DIGITS[to_digits]  if to_digits.is_a? Symbol
-    raise "base must cover digits" if base > digits.length or to_base > to_digits.length
+    raise "base must cover digits." if base > digits.length or to_base > to_digits.length
     @base, @to_base, @digits, @to_digits = base, to_base, digits, to_digits
   end
  
@@ -19,7 +19,7 @@ class FromTo
     when String
       tob(toi(counter), @to_base, @to_digits)
     else
-      raise "counter must be String|Integer"
+      raise "counter must be String|Integer."
     end
   end
   alias :[] :convert

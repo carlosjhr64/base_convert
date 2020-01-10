48498e6f5ee014dfb03c809e567acff51158a998
carlosjhr64 <carlosjhr64@gmail.com>
2020-01-10 08:00:51 -0800

    configuration completeness and tests

--- lib/base_convert/configuration.rb
+++ lib/base_convert/configuration.rb
@@ -140,6 +140,8 @@ module BaseConvert
   INDEXa = DIGITS[:P95].index('a')
   # 0123456789ABCDEF
   DIGITS[:hexadecimal] = DIGITS[:hex] = DIGITS[:h] = :P16
+  # 0123456789
+  DIGITS[:decimal] = DIGITS[:dec] = :P10
   # 01234567
   DIGITS[:octal] = DIGITS[:oct] = DIGITS[:o] = :P8
   # 01
@@ -152,7 +154,7 @@ module BaseConvert
   DIGITS[:Q91] = DIGITS[:qgraph] = DIGITS[:q] = :'graph-quotes'
 
   # ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/
-  DIGITS[:base64] = DIGITS[:B64] = :LN_k43k47
+  DIGITS[:base64] = DIGITS[:b64] = DIGITS[:B64] = :LN_k43k47
   DIGITS[:letters] = DIGITS[:l] = :B52 # subset of B64
 
   # 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz
@@ -195,15 +197,45 @@ module BaseConvert
   end
 
   BASE = {
-    graph:       94,  g:   94,
-    qgraph:      91,  q:   91,
-    word:        63,  w:   63,
-    letters:     52,  l:   52,
-    unambiguous: 47,  u:   47,
-    hexadecimal: 16,  hex: 16,  h: 16,
-    decimal:     10,  dec: 10,  d: 10,
-    octal:        8,  oct:  8,  o:  8,
-    binary:       2,  bin:  2,  b:  2,
+    # 95
+    P95: 95,
+    print: 95,
+    # 94
+    G94: 94, g: 94, graph: 94,
+    # 91
+    Q91: 91, qgraph: 91, q: 91,
+    # 64
+    B64: 64, base64: 64, b64: 64,
+    # 63
+    W63: 63, word: 63, w: 63,
+    # 52
+    letters: 52, l: 52, L: 52,
+    # 47
+    U47: 47, unambiguous: 47, u: 47,
+    # 16
+    hexadecimal: 16, hex: 16, h: 16,
+    # 15
+    ambiguous: 15,
+    # 10
+    decimal: 10, dec: 10, d: 10,
+    # 9
+    operators: 9,
+    # 8
+    octal: 8, oct: 8, o: 8,
+    # 6
+    groupers: 6,
+    # 5
+    separators: 5,
+    # 4
+    quoters: 4,
+    # 3
+    typers: 3,
+    quotes: 3,
+    # 2
+    binary: 2, bin: 2, b: 2,
+    bangs: 2,
+    scapes: 2,
+    spacers: 2,
   }
 
   def BASE.[](key)
